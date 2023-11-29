e5a09e94e02d2bd1c227d869590514202844ec58
Sualeh Fatehi <sualeh@hotmail.com>
2023-11-29 16:12:17 -0500

    Add settings and clean up code
    Signed-off-by: Sualeh Fatehi <sualeh@hotmail.com>

--- src/main/java/us/fatehi/leeting/ValidParentheses.java
+++ src/main/java/us/fatehi/leeting/ValidParentheses.java
@@ -4,48 +4,49 @@ import java.util.Stack;
 
 public class ValidParentheses {
 
-        public static void main(String args[]) {
-            ValidParentheses main = new ValidParentheses();
+    public static void main(String args[]) {
+        ValidParentheses main = new ValidParentheses();
 
-            System.out.println("\"" + "([)]" + "\" --> " + main.isValid("([)]"));
-            
-            System.out.println("\"" + "()[]{}" + "\" --> " + main.isValid("()[]{}"));
+        System.out.println("\"" + "([)]" + "\" --> " + main.isValid("([)]"));
 
-            System.out.println("\"" + "([])" + "\" --> " + main.isValid("([])"));
-        }
+        System.out.println("\"" + "()[]{}" + "\" --> " + main.isValid("()[]{}"));
 
-        public boolean isValid(String s) {
-            System.out.println("new case=" + s);
-            if (s == null)  return true;
-            Stack<Character> bracket = new Stack<Character>();
-            for (int i = 0; i < s.length(); i++) {
-                char currchar = s.charAt(i);
-                switch (currchar) {
-                    case '{':
-                    case '[':
-                    case '(':
-                        bracket.push(currchar);
-                        break;
-                    case '}':
-                    case ']':
-                    case ')':
-                        if (bracket.isEmpty()) {
-                            return false;
-                        }  
-                        char lastopen = bracket.pop();
-                        System.out.println("open=" + lastopen + " close=" + currchar);
-                        if (currchar == '}' && lastopen != '{') {
-                            return false;
-                        } 
-                        if (currchar == ']' && lastopen != '[') {
-                            return false;
-                        } 
-                        if (currchar == ')' && lastopen != '(') {
-                            return false;
-                        }             
-                        break;
-                } 
-            }
-            return bracket.isEmpty();
-        }
+        System.out.println("\"" + "([])" + "\" --> " + main.isValid("([])"));
     }
+
+    public boolean isValid(String s) {
+        System.out.println("new case=" + s);
+        if (s == null)
+            return true;
+        Stack<Character> bracket = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char currchar = s.charAt(i);
+            switch (currchar) {
+                case '{':
+                case '[':
+                case '(':
+                    bracket.push(currchar);
+                    break;
+                case '}':
+                case ']':
+                case ')':
+                    if (bracket.isEmpty()) {
+                        return false;
+                    }
+                    char lastopen = bracket.pop();
+                    System.out.println("open=" + lastopen + " close=" + currchar);
+                    if (currchar == '}' && lastopen != '{') {
+                        return false;
+                    }
+                    if (currchar == ']' && lastopen != '[') {
+                        return false;
+                    }
+                    if (currchar == ')' && lastopen != '(') {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return bracket.isEmpty();
+    }
+}
