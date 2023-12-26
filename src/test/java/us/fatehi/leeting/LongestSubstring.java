a1fb526179e59a24e1c3b8b520ab66577a8f9959
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-26 16:47:59 -0500

    Fix tests
    Signed-off-by: Sualeh Fatehi <sualeh@hotmail.com>

--- src/test/java/us/fatehi/leeting/LongestSubstring.java
+++ src/test/java/us/fatehi/leeting/LongestSubstring.java
@@ -14,15 +14,42 @@ public class LongestSubstring {
         LongestSubstring longestSubstring = new LongestSubstring();
 
         String longestSubstringValue;
-        
-        longestSubstringValue = longestSubstring.longestSubstring("");
-        Assertions.assertEquals(true, "".equals(longestSubstringValue));
 
         longestSubstringValue = longestSubstring.longestSubstring("");
-        Assertions.assertEquals(true, "".equals(longestSubstringValue));
+        Assertions.assertEquals("", longestSubstringValue, "Empty String");
+
+        longestSubstringValue = longestSubstring.longestSubstring("a");
+        Assertions.assertEquals("a", longestSubstringValue,
+                "String with a Single Character");
+
+        longestSubstringValue = longestSubstring.longestSubstring("abcdef");
+        Assertions.assertEquals("abcdef", longestSubstringValue,
+                "String with All Unique Characters");
+
+        longestSubstringValue = longestSubstring.longestSubstring("abcabcbb");
+        Assertions.assertEquals("abc", longestSubstringValue,
+                "String with Repeating Characters");
+
+        longestSubstringValue = longestSubstring.longestSubstring("bbabcdef");
+        Assertions.assertEquals("abcdef", longestSubstringValue,
+                "String with Repeating Characters at the Beginning");
+
+        longestSubstringValue = longestSubstring.longestSubstring("abcdeff");
+        Assertions.assertEquals("abcdef", longestSubstringValue,
+                "String with Repeating Characters at the End");
+
+        longestSubstringValue = longestSubstring.longestSubstring("aaaa");
+        Assertions.assertEquals("a", longestSubstringValue,
+                "String with Only Repeating Characters");
     }
 
-    private String longestSubstring(String string) {
+    private String longestSubstring(final String string) {
+        if (string == null || string.length() == 0) {
+            return "";
+        }
+        if (string.length() == 1) {
+            return string;
+        }
         return null;
     }
 
