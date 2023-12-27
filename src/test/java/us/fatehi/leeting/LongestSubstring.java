3eb8373e12b5be14eced39dff38c25711ef9e565
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-27 10:24:02 -0500

    Complete algorithm for longest substring
    Signed-off-by: Sualeh Fatehi <sualeh@hotmail.com>

--- src/test/java/us/fatehi/leeting/LongestSubstring.java
+++ src/test/java/us/fatehi/leeting/LongestSubstring.java
@@ -1,5 +1,9 @@
 package us.fatehi.leeting;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -45,10 +49,34 @@ public class LongestSubstring {
         if (string == null || string.length() == 0) {
             return "";
         }
-        if (string.length() == 1) {
-            return string;
+        System.out.println(">           " + string);
+        final Set<Integer> foundCodePoints = new HashSet<>();
+        int start = 0;
+        int end = 1;
+        int longest_start = 0;
+        int longest_end = 1;
+        foundCodePoints.add(string.codePointAt(0));
+        while (end < string.length()) {
+            int codePointAtEnd = string.codePointAt(end);
+            if (foundCodePoints.contains(codePointAtEnd)) {
+                // Move the start pointer to the matching character
+                while (string.codePointAt(start) != codePointAtEnd) {
+                    start = start + 1;
+                }
+                // Now the matching character is at the start of the substring,
+                // so advance and eliminate it
+                start = start + 1;
+            }
+            foundCodePoints.add(codePointAtEnd);
+            end = end + 1;
+            if ((end - start) > (longest_end - longest_start)) {
+                longest_start = start;
+                longest_end = end;
+            }
+            System.out.printf("- [%2d - %2d] %s {window %s}%n", longest_start, longest_end,
+                    string.substring(longest_start, longest_end), string.substring(start, end));
         }
-        return null;
+        return string.substring(longest_start, longest_end);
     }
 
 }
