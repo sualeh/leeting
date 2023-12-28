5c0289e98c5d849d2062f49997ba0d0a3376736c
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-28 13:42:05 -0500

    Move reverse linked list
    Signed-off-by: Sualeh Fatehi <sualeh@hotmail.com>

--- src/test/java/us/fatehi/leeting/problems/ProblemReverseLinkedList.java
+++ src/test/java/us/fatehi/leeting/problems/ProblemReverseLinkedList.java
@@ -1,4 +1,4 @@
-package us.fatehi.leeting;
+package us.fatehi.leeting.problems;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,12 +11,14 @@ import us.fatehi.leeting.util.ListNode;
  * 
  * https://leetcode.com/problems/reverse-linked-list/
  */
-public class ReverseLinkedList {
+public abstract class ProblemReverseLinkedList implements Problem {
+
+    public abstract ListNode reverseList(ListNode head);
 
     @Test
     public void maintest() {
 
-        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+        ProblemReverseLinkedList reverseLinkedList = (ProblemReverseLinkedList) solutionObject();
 
         ListNode reversedList;
 
@@ -36,43 +38,6 @@ public class ReverseLinkedList {
         reversedList = reverseLinkedList.reverseList(ListNode.of(1, 2, 3, 4));
         Assertions.assertEquals(ListNode.of(4, 3, 2, 1), reversedList,
                 "Happy path, even number of nodes");
-    }
-
-    public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        System.out.println(">> Given: " + head);
-        ListNode c1 = head;
-        ListNode c2 = nextNode(c1);
-        ListNode c3 = nextNode(c2);
-        ListNode revHead = c1;
-
-        do {
-            if (c2 != null) {
-                if (c1 == head) {
-                    c1.next = null;
-                }
-                c2.next = c1;
-                revHead = c2;
-                System.out.println("   Built: " + revHead);
-                // Move along the list
-                c1 = c2;
-                c2 = c3;
-                c3 = nextNode(c2);
-            } else {
-                break;
-            }
-        } while (true);
-        return revHead;
-    }
-
-    private ListNode nextNode(ListNode head) {
-        if (head == null) {
-            return null;
-        } else {
-            return head.next;
-        }
     }
 
 }
