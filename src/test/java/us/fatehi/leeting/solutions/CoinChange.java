43cdc42fd472a827a38604ee5882f42d4f9cd41e
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-28 13:31:31 -0500

    Convert coin change problem
    Signed-off-by: Sualeh Fatehi <sualeh@hotmail.com>

---
+++ src/test/java/us/fatehi/leeting/solutions/CoinChange.java
@@ -0,0 +1,16 @@
+package us.fatehi.leeting.solutions;
+
+import us.fatehi.leeting.problems.ProblemCoinChange;
+
+
+public class CoinChange extends ProblemCoinChange {
+
+    public int coinChange(int[] coins, int amount) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public ProblemCoinChange solutionObject() {
+        return new CoinChange();
+    }
+}
