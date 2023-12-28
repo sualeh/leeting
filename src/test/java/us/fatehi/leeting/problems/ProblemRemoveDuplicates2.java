76982467474e0cee21fad4001da2b0b57a77495d
Sualeh Fatehi <sualeh@hotmail.com>
2023-12-28 13:48:59 -0500

    Move last problem
    Signed-off-by: Sualeh Fatehi <sualeh@hotmail.com>

---
+++ src/test/java/us/fatehi/leeting/problems/ProblemRemoveDuplicates2.java
@@ -0,0 +1,27 @@
+package us.fatehi.leeting.problems;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import us.fatehi.leeting.util.ListNode;
+
+/**
+ * Remove Duplicates from Sorted List II
+ * 
+ * Linked List
+ * 
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
+ */
+public abstract class ProblemRemoveDuplicates2 implements Problem {
+
+    @Test
+    public void maintest() {
+
+        ProblemRemoveDuplicates2 dedupe = (ProblemRemoveDuplicates2) solutionObject();
+
+        ListNode deduped1 = dedupe.deleteDuplicates(ListNode.of(1, 1, 1, 2, 3));
+        Assertions.assertEquals(deduped1, ListNode.of(2, 3));
+    }
+
+    public abstract ListNode deleteDuplicates(ListNode head);
+
+}
